acb0ce4ea9270dd56ca99e49bfe02a59c5978b4e
baristunar <tunarbaris7@gmail.com>
2021-04-25 23:26:24 +0300

    Add untranslated  forgetten data

--- src/components/Navbar/appMobileMenuUserInfo.vue
+++ src/components/Navbar/appMobileMenuUserInfo.vue
@@ -2,7 +2,9 @@
   <v-list-item two-line>
     <v-list-item-content>
       <v-list-item-title>{{ activeUser.email }}</v-list-item-title>
-      <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+      <v-list-item-subtitle>{{
+        $t("information.loggedIn")
+      }}</v-list-item-subtitle>
     </v-list-item-content>
   </v-list-item>
 </template>
